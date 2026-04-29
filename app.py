8fef77d5d2442cccb5d5dd43f127e93225c546ea
Your Name <youremail@gmail.com>
2026-04-29 23:58:24 +0530

    Fixed port binding for Render

--- app.py
+++ app.py
@@ -246,5 +246,5 @@ def toggle_user_status(id):
 
 #Run Flask Server
 if __name__ == "__main__":
-    app.run(debug=True, port=5000, use_reloader=False)
-
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
